d875b3d4415d987048c0bc5c2159564868ff2f53
SusejR <rodriguezbsusej@gmail.com>
2020-07-18 06:23:30 -0400

    Updates

---
+++ precache-manifest.8936cbed5cca1a8a824e145f8884b203.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cca05858677c16db532012cf4693783e",
+    "url": "/makeYourDrink/index.html"
+  },
+  {
+    "revision": "75b99c7970f9e95ffd23",
+    "url": "/makeYourDrink/static/css/main.59fb0f1e.chunk.css"
+  },
+  {
+    "revision": "f5bce182ced4b658c5a8",
+    "url": "/makeYourDrink/static/js/2.115fc905.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/makeYourDrink/static/js/2.115fc905.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75b99c7970f9e95ffd23",
+    "url": "/makeYourDrink/static/js/main.b81eb4d8.chunk.js"
+  },
+  {
+    "revision": "1af863fc385a1446196a",
+    "url": "/makeYourDrink/static/js/runtime-main.03f6fe26.js"
+  }
+]);
